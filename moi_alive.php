0c95db0a9d7263ac3b7c949b5e2d8db97a852f3b
Jason HSU <kids626@gmail.com>
2025-02-08 00:59:01 +0800

    加入mrg2代理伺服器

--- moi_alive.php
+++ moi_alive.php
@@ -4,17 +4,13 @@
 //echo '<meta http-equiv="refresh" content="5">';
 
 
-$protectedUrl = "https://moi-mgr2.moi.gov.tw/SiteLobby.aspx";
-$session_value ='diuyvx4qqzj00nho4lgvofyz';
-//$session_value =$_GET['sid']; 
+$protectedUrl = "https://moi-mgr.moi.gov.tw/SiteLobby.aspx";
+//$session_value ='diuyvx4qqzj00nho4lgvofyz';
+$session_value =$_GET['sid']; 
 $sessionId = "ASP.NET_SessionId=$session_value"; // 直接帶入 Session ID
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $protectedUrl);
-curl_setopt($ch, CURLOPT_PROXY, "http://43.224.248.42:3128"); // 設定 Proxy
-//curl_setopt($ch, CURLOPT_PROXY, "http://34.81.9.136:3128"); // 設定 Proxy
-//curl_setopt($ch, CURLOPT_PROXY, "http://1.160.3.107:8081"); // 設定 Proxy
-//curl_setopt($ch, CURLOPT_PROXY, "http://114.35.140.157:8080"); // 設定 Proxy
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ["Cookie: $sessionId"]);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
